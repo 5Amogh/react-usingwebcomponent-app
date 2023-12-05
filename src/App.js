08f8edd01468f5a4223e6132feb592e1e8b7d0b1
5Amogh <amoghavarsh@navadhiti.com>
2023-12-05 15:26:37 +0530

    Integration till file upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./App.css";
-import React, { useRef, useEffect } from "react";
-import "./web-component/questionnaire-player-webcomponent.js";
+import React, { useRef, useEffect, useState} from "react";
+import "questionnaire-webcomponent/questionnaire-player-webcomponent.js";
 import "./web-component/styles.css";
 import mockData from "./data.json";
 function App() {
   const questionairePlayerMainRef = useRef(null);
-  const assessment = mockData
+  const [fileUploadResponse, setFileUploadResponse] = useState(null);
+  const assessment = mockData;
+  const receiveUploadData = (event) => {
+    if (event.data.question_id) {
+    const obj = {
+      name: event.data.name,
+      url: URL.createObjectURL(event.data.file)
+    };
+    setFileUploadResponse({status:200, data:obj, question_id:event.data.question_id});
+  }
+  }
+
+  useEffect(() => {
+    window.addEventListener('message', receiveUploadData.bind(this), false);
+
+    return () => {
+      window.removeEventListener('message', receiveUploadData.bind(this), false);
+    };
+  }, []);
+
   useEffect(() => {
     const playerElement = questionairePlayerMainRef.current;
+    
     const handlePlayerSubmitOrSaveEvent = (event) => {
       console.log("Event Data Logged from the react app", event.detail);
     };
-
+    
     playerElement.addEventListener(
       "submitOrSaveEvent",
       handlePlayerSubmitOrSaveEvent
@@ -30,6 +50,7 @@ function App() {
     <div>
       <questionnaire-player-main
         assessment={JSON.stringify(assessment)}
+        fileuploadresponse={JSON.stringify(fileUploadResponse)}
         ref={questionairePlayerMainRef}
       ></questionnaire-player-main>
     </div>
